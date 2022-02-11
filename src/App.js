39c7fcb88fe9c0bac1225acd3e2416511c2dce65
Akash <adesigns>
2022-02-11 12:59:09 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {AppBar, Typography} from "@mui/material";
+import useStyles from "./styles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const classes = useStyles();
+
+    return (
+        <AppBar className = {classes.appbar}>
+              <Typography variant = "h4"> API  </Typography>
+        </AppBar>
+    )
 }
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+
+    appbar: {
+        padding: "5px 5px",
+        background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+        boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+        height: 50,
+    },
+});
+
+export default useStyles;
